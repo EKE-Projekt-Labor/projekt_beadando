5774b036829de0822a80853a1f520006c28c7c59
7OMI <esztomi91@gmail.com>
2020-01-20 09:32:36 +0100

    lekérdező fv kiegészítve

--- Web/inc/php/db_query.php
+++ Web/inc/php/db_query.php
@@ -11,19 +11,30 @@
 	* @param	bool	$retArray	tömbbel térjen vissza?
 	*/
 
-	function db_query ($sql, $retArray) {
+	function db_query ($sql, $retArray=true) {
 		global $link;
 
 		if ($result = $link->query($sql)) {
 
 		    if (preg_match('/^SELECT/msi', $sql)) {
 
-				//-- ha adatot kérdezünk le, akkor adjon vissza egy tömböt vagy stringet
+				while ($row = $result->fetch_assoc()) {
+					if ($retArray) {
+						$res[] = $row;
+					} else {
+						$singl = preg_replace('/^.*?SELECT[\s]{1,}([a-zA-Z0-9_-]{1,})(?:,|[\s]{1,}).*?$/m',
+							'$1', $sql); // az első oszlop neve
+						$res = $row[$singl];
+					}
+				}
+
+				$result->free();
 
 			}
 
 		}
 
+		return $res;
 	}
 
 ?>
