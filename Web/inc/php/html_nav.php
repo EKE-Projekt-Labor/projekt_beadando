52d52471b866d71d8f0bf433fa38520a08dd015f
Milán Kormány <kormany.1993@gmail.com>
2020-01-21 12:24:26 +0100

    dropdown menu, dropdown items és záró tagek

--- Web/inc/php/html_nav.php
+++ Web/inc/php/html_nav.php
@@ -26,7 +26,40 @@
           <li class="nav-item"><a class="nav-link" href="curriculum.php">Tananyagok</a></li>
           <li class="nav-item"><a class="nav-link" href="test.php">Tesztek</a></li>
         </ul>
-	HTML;
+		<div class="form-inline my-2 my-md-0">
+          <ul class="navbar-nav mr-auto">
+
+            ${!${''}=($_SESSION["loggedin"]?'
+              <li class="nav-item dropdown mr-2">
+                <a class="btn btn-secondary dropdown-toggle" href="#" data-toggle="dropdown"><img src="inc/img/icons/gear.svg" alt="" width="24" height="24"> Beállítások</a>
+                <div class="dropdown-menu dropdown-menu-right">
+                  <a class="dropdown-item" href="reset-password.php">Új jelszó</a>
+
+                  '.(user_perm()>=5?'
+                    <div class="dropdown-divider"></div>
+                    <h6 class="dropdown-header">Adminisztráció</h6>
+                    <a class="dropdown-item" href="admin_user.php">Felhasználók</a>
+                    <a class="dropdown-item" href="admin_userclass.php">Osztályok</a>
+                    <a class="dropdown-item" href="admin_category.php">Kategóriák</a>
+                  ':'').'
+
+                </div>
+              </li>
+            ':'
+              <li class="nav-item mr-2"><a class="btn btn-warning" href="register.php">Regisztráció</a></li>
+            ')}
+
+            ${!${''}=($_SESSION["loggedin"]?'
+              <li class="nav-item"><a class="btn btn-danger" href="logout.php">Kijelentkezés</a></li>
+            ':'
+              <li class="nav-item"><a class="btn btn-success" href="login.php">Bejelentkezés</a></li>
+            ')}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+HTML;
 
 }
 
