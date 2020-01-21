79539531e265b1852545c6fdc3c83d01aee13b60
MrVeres <mrveres95@gmail.com>
2020-01-21 18:04:17 +0100

    register pw validation

--- Web/register.php
+++ Web/register.php
@@ -42,6 +42,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
          
         mysqli_stmt_close($stmt);
     }
+	
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Add meg a jelszavad.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "A jelszónak minimum 6 karakternek kell lennie.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
 
     /**
     * Hiba kezelés, adatbázisba feltöltés
