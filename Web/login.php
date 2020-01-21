4c4b09d9c5949c1051d99bc719bfc61734bc4414
albachzsolt <albach.zsolt1@gmail.com>
2020-01-21 17:11:32 +0100

    Login.php session start + user log check

---
+++ Web/login.php
@@ -0,0 +1,21 @@
+	<?php
+ /**
+* Login.PHP
+*
+* @author   __Albach Zsolt__
+*/
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("location: index.php");
+  exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+?>
