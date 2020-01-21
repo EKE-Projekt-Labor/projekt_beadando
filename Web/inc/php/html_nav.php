de7f30c38d9b430f6f34a9c0007570e8f79d385f
Milán Kormány <kormany.1993@gmail.com>
2020-01-21 12:16:34 +0100

    html_nav.php funkció kialakitása

--- Web/inc/php/html_nav.php
+++ Web/inc/php/html_nav.php
@@ -8,6 +8,16 @@
 	* @param	string	$activePage		aktív oldal neve
 	* @return	string	Navigáció.
     */
-    
+    function html_nav ($activePage) {
+
+		$userPerm = user_perm();
+	
+			return 
+	<<<HTML
+	
+	HTML;
+
+}
+
 
 ?>
