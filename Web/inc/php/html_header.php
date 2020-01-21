ec213029eac7abe26ea67f71ed392f0c52fe24d0
MrVeres <mrveres95@gmail.com>
2020-01-21 17:42:13 +0100

    html header create

---
+++ Web/inc/php/html_header.php
@@ -0,0 +1,16 @@
+<?php
+
+	/**
+	* HTML: FEJLÉC
+	*
+	* Beleértve a <head> blokkot és a vizuálisan is megjelenő fejlécet,
+	* ami már a <body> tag-en belül szerepel.
+	*
+	* @author	Veres Tamás
+	*
+	* @param	string	$title	cím
+	* @param	array	$datas	adatok
+	* @return	string	Fejléc.
+	*/
+
+?>
