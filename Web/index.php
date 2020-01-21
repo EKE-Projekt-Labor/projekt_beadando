830fcf8c2d8ca6bf79faa814ac54da6e40d1eac5
7OMI <esztomi91@gmail.com>
2020-01-21 14:15:51 +0100

    teszt index megírása

--- Web/index.php
+++ Web/index.php
@@ -1,5 +1,27 @@
 <?php
 
-echo 'Helló világ! :)';
+/**
+* Index.PHP
+*
+* @author   Eszényi Tamás
+*/
 
+session_start();
+require 'config.php';
+ 
+
+echo html_header ('Üdvözlet <b>'.htmlspecialchars($_SESSION["username"]).'</b>!');
+
+?>
+
+    <p>
+    <?php
+
+        #kiírás
+
+    ?>
+    </p>
+
+<?php
+    echo html_footer();
 ?>
