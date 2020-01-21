f6b6e5b2aa5df45d49fe8dbfac78af49ef26301c
MrVeres <mrveres95@gmail.com>
2020-01-21 18:00:19 +0100

    register validation modification

--- Web/register.php
+++ Web/register.php
@@ -13,13 +13,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     * @author   Veres Tamás
     */
 
-        // Validate username
     if(empty(trim($_POST["username"]))){
         $username_err = "Felhasználónév megadása szükséges.";
     } else{
-        // Prepare a select statement
+
         $sql = db_sql('user:nameCheck');
         
+		if($stmt = mysqli_prepare($link, $sql)){
+
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+
+            $param_username = trim($_POST["username"]);
+            
+            if(mysqli_stmt_execute($stmt)){
+
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "A felhasználónév már használatban van.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Hiba! Próbáld meg később.";
+            }
+        }
+         
+        mysqli_stmt_close($stmt);
     }
 
     /**
