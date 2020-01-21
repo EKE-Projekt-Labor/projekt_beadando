e05aefee433671c0ab29ab51f3e1dd4734d60104
Milán Kormány <kormany.1993@gmail.com>
2020-01-21 12:21:07 +0100

    navbar-toggler és icon

--- Web/inc/php/html_nav.php
+++ Web/inc/php/html_nav.php
@@ -17,7 +17,9 @@
 	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container">
       <a class="navbar-brand" href="index.php">E-LEARNING</a>
-	  
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 	HTML;
 
 }
