f59c39201cddc32384bfa008eb8e35e2d6a80a2d
Milán Kormány <kormany.1993@gmail.com>
2020-01-21 12:19:25 +0100

    nav bar hozzá adása

--- Web/inc/php/html_nav.php
+++ Web/inc/php/html_nav.php
@@ -14,7 +14,10 @@
 	
 			return 
 	<<<HTML
-	
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">E-LEARNING</a>
+	  
 	HTML;
 
 }
