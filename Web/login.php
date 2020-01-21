a89dae225d1e2136f6d5ebb0a11008e853067686
albachzsolt <albach.zsolt1@gmail.com>
2020-01-21 17:46:33 +0100

    login.php HTML- form elkészítése

--- Web/login.php
+++ Web/login.php
@@ -36,3 +36,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $password = trim($_POST["password"]);
     }
 ?>
+<p></p>
+        <form class="c" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Felhasználónév</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Jelszó</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Bejelentkezés">
+            </div>
+            <p>Nincs még fiókod? <a href="register.php">Regisztráció</a>.</p>
+        </form>
+
+        <pre>
+       Felhasználók:
+        - felh      (jelszó: 123456)
+        - diak     (jelszó: titok123)
+        - diak2     (jelszó: 123456)
+        - tanar     (jelszó: 123456)
+        - tanar2    (jelszó: 123456)
+        - admin     (jelszó: 123456)
+        - admin2    (jelszó: 123456)
+        </pre>
