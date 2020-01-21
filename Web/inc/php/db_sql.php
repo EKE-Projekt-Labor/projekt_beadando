824839160ac8ca74fcd9183f55687173371eed93
Milán Kormány <kormany.1993@gmail.com>
2020-01-21 20:01:59 +0100

    db_sql.php - user:classid

--- Web/inc/php/db_sql.php
+++ Web/inc/php/db_sql.php
@@ -32,6 +32,9 @@
 			case 'user:permission': 
 				return "SELECT permission FROM user WHERE id = ".$_SESSION["id"]; break;
 
+			case 'user:classid': 
+				return "SELECT classid FROM user WHERE id = ".$_SESSION["id"]; break;
+				
 			case 'user:nameCheck': 
 				return "SELECT id FROM user WHERE username = ?"; break; 
 			
