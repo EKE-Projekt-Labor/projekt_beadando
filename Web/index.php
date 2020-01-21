2ac5bb5d92f6927185f6dc02d3a25101f7c70125
7OMI <esztomi91@gmail.com>
2020-01-21 14:18:24 +0100

    kiírások megírva

--- Web/index.php
+++ Web/index.php
@@ -17,7 +17,21 @@ echo html_header ('Üdvözlet <b>'.htmlspecialchars($_SESSION["username"]).'</b>
     <p>
     <?php
 
-        #kiírás
+        // Átlagos felhasználó
+        if ($_SESSION["loggedin"] && user_perm() > 0) {
+            echo "Használd a fenti menüpontot a Tananyagok, Tesztek és Játékok eléréshez.";
+        }
+
+        // Joggal rendelkező felhasználó
+        else if ($_SESSION["loggedin"] && user_perm() === 0) {
+            echo "Jelenleg csak sima felhasználó vagy, nem érhetők el számodra a szolgáltatások.<br>";
+            echo "Kérj meg egy tanárt vagy admint, hogy adják meg neked a megfelelő jogosultságot!";
+        } 
+
+        // Látogató
+        else {
+            echo 'Az oldal használatához egy regisztrált fiókra van szükség.';
+        }
 
     ?>
     </p>
