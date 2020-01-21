f7de6f0553d3bb468c75bbe67f0915f4415c81ab
7OMI <esztomi91@gmail.com>
2020-01-21 14:25:23 +0100

    reg váz

---
+++ Web/register.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "config.php";
+ 
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    /**
+    * Validálás
+    *
+    * @author   Veres Tamás
+    */
+
+    # kód
+
+    /**
+    * Hiba kezelés, adatbázisba feltöltés
+    *
+    * @author   Kormány Milán
+    */
+
+    # kód
+
+}
+
+
+/**
+* Regisztrációs űrlap
+*
+* @author   Veres Tamás
+*/
+
+# kód
+
+?>
