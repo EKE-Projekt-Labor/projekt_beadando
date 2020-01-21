4359dcf5e05d524d5e4d96e78145e46e881d5185
7OMI <esztomi91@gmail.com>
2020-01-21 15:07:33 +0100

    új jelszó php váz

---
+++ Web/reset-password.php
@@ -0,0 +1,35 @@
+<?php require 'config.php'; user_perm(0);
+
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    /**
+    * Validálás
+    *
+    * @author   Veres Tamás
+    */
+
+    #kód
+
+
+    /**
+    * Hibakezelés, frissítés az adatbézisban
+    *
+    * @author   Kormány Milán
+    */
+
+    # kód
+
+}
+
+/**
+* Új jelszó űrlap
+*
+* @author   Veres Tamás
+*/
+
+# kód
+
+?>
